2b672de60f097e93cf303ceda164b44425138712
T Sri Skanthan <sriskanthan2006@gmail.com>
2025-05-17 20:01:25 +0530

    Delete Phase-1/Session-18/angular-app directory

--- Phase-1/Session-18/angular-app/src/app/CustomPipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from "@angular/core";
-
-@Pipe({name:'changeString'})
-export class CustomPipe implements PipeTransform{
-
-    transform(value: string, character: string) {
-        return value.split(character).join("-")        
-    }
-}
